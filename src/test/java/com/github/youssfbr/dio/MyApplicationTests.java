793e713e218bbc9af15b080acab52684345b9605
Alisson Youssf <youssfbr@gmail.com>
2023-10-16 00:16:12 -0300

    Project created

---
+++ src/test/java/com/github/youssfbr/dio/MyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.dio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
