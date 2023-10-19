8b20388e99cd06428fd3119d7990a1f9d1d54bc0
Alisson Youssf <youssfbr@gmail.com>
2023-10-18 22:54:31 -0300

    CORS

--- src/main/java/com/github/youssfbr/dio/MyApplication.java
+++ src/main/java/com/github/youssfbr/dio/MyApplication.java
@@ -1,9 +1,12 @@
 package com.github.youssfbr.dio;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = { @Server(url = "/", description = "Default Server URL")})
 public class MyApplication {
 
 	public static void main(String[] args) {
